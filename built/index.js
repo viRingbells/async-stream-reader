076f1073aadae647557d051d303769b5efb9d37e
chris <chris@divvito.com>
2018-03-09 10:44:58 +1000

    convert to typescript - tests left in JS but still passing :)

--- built/index.js
+++ built/index.js
@@ -7,46 +7,27 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [0, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+import * as assert from 'assert';
+import * as Debug from 'debug';
+import * as Stream from 'stream';
+import * as EventEmitter from 'events';
+const debug = Debug('async-stream-reader');
+const DATA = Symbol('READER DATA');
+const END = Symbol('READER END');
+const ERROR = Symbol('READER ERROR');
+function isFunction(test) {
+    return test && test instanceof Function;
+}
+class Reader {
+    static get ERROR() {
+        return ERROR;
     }
-};
-var assert = require('assert');
-var debug = require('debug')('async-stream-reader');
-var stream = require('stream');
-var EventEmitter = require('events');
-var DATA = Symbol('READER DATA');
-var END = Symbol('READER END');
-var ERROR = Symbol('READER ERROR');
-var Reader = /** @class */ (function () {
-    function Reader(readable, options) {
-        if (options === void 0) { options = {}; }
-        var _this = this;
+    static get END() {
+        return END;
+    }
+    constructor(readable, options = {}) {
         debug('construct');
-        assert(readable instanceof stream.Readable || readable instanceof EventEmitter, 'Only support stream.Readable or EventEmitter');
+        assert(readable instanceof Stream.Readable || readable instanceof EventEmitter, 'Only support Stream.Readable or EventEmitter');
         this.readable = readable;
         this.eventHolder = [];
         this._pause = options.pause || 'pause';
@@ -54,144 +35,85 @@ var Reader = /** @class */ (function () {
         this._isPaused = options.isPaused || 'isPaused';
         this._endArgument = options.end || undefined;
         this._emitter = new EventEmitter();
-        var events = options.events || {};
-        var dataEvents = events.data || 'data';
-        var endEvents = events.end || 'end';
-        var errorEvents = events.error || 'error';
+        const events = options.events || {};
+        let dataEvents = events.data || 'data';
+        let endEvents = events.end || 'end';
+        let errorEvents = events.error || 'error';
         dataEvents = Array.isArray(dataEvents) ? dataEvents : [dataEvents];
         endEvents = Array.isArray(endEvents) ? endEvents : [endEvents];
         errorEvents = Array.isArray(errorEvents) ? errorEvents : [errorEvents];
         this.pause();
-        var _loop_1 = function (name_1) {
-            this_1.readable.on(name_1, function () {
-                var args = [];
-                for (var _i = 0; _i < arguments.length; _i++) {
-                    args[_i] = arguments[_i];
-                }
-                return _this._emitter.emit('any', { name: name_1, args: args });
-            });
-        };
-        var this_1 = this;
-        for (var _i = 0, _a = dataEvents.concat(endEvents, errorEvents); _i < _a.length; _i++) {
-            var name_1 = _a[_i];
-            _loop_1(name_1);
+        for (const name of [...dataEvents, ...endEvents, ...errorEvents]) {
+            this.readable.on(name, (...args) => this._emitter.emit('any', { name, args }));
         }
-        for (var _b = 0, dataEvents_1 = dataEvents; _b < dataEvents_1.length; _b++) {
-            var name_2 = dataEvents_1[_b];
-            this.readable.on(name_2, function () {
-                var args = [];
-                for (var _i = 0; _i < arguments.length; _i++) {
-                    args[_i] = arguments[_i];
-                }
-                return _this.eventHolder.push({ type: DATA, args: args });
-            });
+        for (const name of dataEvents) {
+            this.readable.on(name, (...args) => this.eventHolder.push({ type: DATA, args }));
         }
-        for (var _c = 0, endEvents_1 = endEvents; _c < endEvents_1.length; _c++) {
-            var name_3 = endEvents_1[_c];
-            this.readable.on(name_3, function () {
-                var args = [];
-                for (var _i = 0; _i < arguments.length; _i++) {
-                    args[_i] = arguments[_i];
-                }
-                return _this.eventHolder.push({ type: END, args: args });
-            });
+        for (const name of endEvents) {
+            this.readable.on(name, (...args) => this.eventHolder.push({ type: END, args }));
         }
-        for (var _d = 0, errorEvents_1 = errorEvents; _d < errorEvents_1.length; _d++) {
-            var name_4 = errorEvents_1[_d];
-            this.readable.on(name_4, function () {
-                var args = [];
-                for (var _i = 0; _i < arguments.length; _i++) {
-                    args[_i] = arguments[_i];
-                }
-                return _this.eventHolder.push({ type: ERROR, args: args });
-            });
+        for (const name of errorEvents) {
+            this.readable.on(name, (...args) => this.eventHolder.push({ type: ERROR, args }));
         }
     }
-    Object.defineProperty(Reader, "ERROR", {
-        get: function () { return ERROR; },
-        enumerable: true,
-        configurable: true
-    });
-    Object.defineProperty(Reader, "END", {
-        get: function () { return END; },
-        enumerable: true,
-        configurable: true
-    });
-    Reader.prototype.pause = function () {
-        if (this._isPaused && this.readable[this._isPaused] instanceof Function &&
-            this.readable[this._isPaused]()) {
+    isReadableMethod(methodName) {
+        return isFunction(this.readable[methodName]);
+    }
+    callReadableMethod(methodName) {
+        return this.readable[methodName]();
+    }
+    pause() {
+        if (this._isPaused && this.isReadableMethod(this._isPaused) &&
+            this.callReadableMethod(this._isPaused)) {
             return this;
         }
-        if (!(this.readable[this._pause] instanceof Function)) {
-            return this;
+        if (this.isReadableMethod(this._pause)) {
+            debug('pause');
+            this.callReadableMethod(this._pause);
         }
-        debug('pause');
-        this.readable[this._pause]();
         return this;
-    };
-    Reader.prototype.resume = function () {
-        if (this._isPaused && this.readable[this._isPaused] instanceof Function &&
-            !this.readable[this._isPaused]()) {
+    }
+    resume() {
+        if (this._isPaused && this.isReadableMethod(this._isPaused) &&
+            !this.callReadableMethod(this._isPaused)) {
             return this;
         }
-        if (!(this.readable[this._resume] instanceof Function)) {
-            return this;
+        if (this.isReadableMethod(this._resume)) {
+            debug('resume');
+            this.callReadableMethod(this._resume);
         }
-        debug('resume');
-        this.readable[this._resume]();
         return this;
-    };
-    Reader.prototype._emitted = function () {
-        return __awaiter(this, void 0, void 0, function () {
-            var _this = this;
-            return __generator(this, function (_a) {
-                return [2 /*return*/, new Promise(function (resolve) { return _this._emitter.once('any', resolve); })];
-            });
+    }
+    _emitted() {
+        return new Promise(resolve => this._emitter.once('any', resolve));
+    }
+    nextEvent() {
+        return __awaiter(this, void 0, void 0, function* () {
+            debug('next event');
+            if (this.eventHolder.length > 0) {
+                this.pause();
+                return this.eventHolder.shift();
+            }
+            this.resume();
+            yield this._emitted();
+            return yield this.nextEvent();
         });
-    };
-    Reader.prototype.nextEvent = function () {
-        return __awaiter(this, void 0, void 0, function () {
-            return __generator(this, function (_a) {
-                switch (_a.label) {
-                    case 0:
-                        debug('next event');
-                        if (this.eventHolder.length > 0) {
-                            this.pause();
-                            return [2 /*return*/, this.eventHolder.shift()];
-                        }
-                        this.resume();
-                        return [4 /*yield*/, this._emitted()];
-                    case 1:
-                        _a.sent();
-                        return [4 /*yield*/, this.nextEvent()];
-                    case 2: return [2 /*return*/, _a.sent()];
-                }
-            });
+    }
+    next() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { args, type } = yield this.nextEvent();
+            debug('next', args, type);
+            const result = args[0];
+            if (type === Reader.ERROR) {
+                debug('error');
+                throw result;
+            }
+            if (type === Reader.END) {
+                debug('end');
+                return this._endArgument;
+            }
+            return result;
         });
-    };
-    Reader.prototype.next = function () {
-        return __awaiter(this, void 0, void 0, function () {
-            var _a, args, type, result;
-            return __generator(this, function (_b) {
-                switch (_b.label) {
-                    case 0: return [4 /*yield*/, this.nextEvent()];
-                    case 1:
-                        _a = _b.sent(), args = _a.args, type = _a.type;
-                        debug('next', args, type);
-                        result = args[0];
-                        if (type === Reader.ERROR) {
-                            debug('error');
-                            throw result;
-                        }
-                        if (type === Reader.END) {
-                            debug('end');
-                            return [2 /*return*/, this._endArgument];
-                        }
-                        return [2 /*return*/, result];
-                }
-            });
-        });
-    };
-    return Reader;
-}());
+    }
+}
 module.exports = Reader;
---
+++ index.ts
@@ -0,0 +1,155 @@
+'use strict';
+
+import * as assert from 'assert';
+import * as Debug from 'debug';
+import * as Stream from 'stream';
+import * as EventEmitter from 'events';
+
+const debug = Debug('async-stream-reader');
+const DATA = Symbol('READER DATA');
+const END = Symbol('READER END');
+const ERROR = Symbol('READER ERROR');
+
+type Readable = Stream.Readable | EventEmitter;
+type Options = {
+    pause?: string,
+    resume?: string,
+    isPaused?: string,
+    end?: string,
+    events?: {
+        data?: string | string[],
+        end?: string | string[],
+        error?: string | string[],
+    }
+}
+
+type Event = {
+    type: symbol,
+    args: any[]
+}
+
+function isFunction(test: any): test is Function {
+    return test && test instanceof Function;
+}
+
+class Reader {
+    private readable: Readable;
+    private eventHolder: Event[];
+    private _pause: string;
+    private _resume: string;
+    private _isPaused: string;
+    private _endArgument: any;
+    private _emitter: EventEmitter;
+
+    static get ERROR(): symbol {
+        return ERROR;
+    }
+
+    static get END(): symbol {
+        return END;
+    }
+
+    constructor(readable: Readable, options: Options = {}) {
+        debug('construct');
+        assert(readable instanceof Stream.Readable || readable instanceof EventEmitter,
+            'Only support Stream.Readable or EventEmitter');
+        this.readable = readable;
+        this.eventHolder = [];
+        this._pause = options.pause || 'pause';
+        this._resume = options.resume || 'resume';
+        this._isPaused = options.isPaused || 'isPaused';
+        this._endArgument = options.end || undefined;
+        this._emitter = new EventEmitter();
+
+        const events = options.events || {};
+        let dataEvents = events.data || 'data';
+        let endEvents = events.end || 'end';
+        let errorEvents = events.error || 'error';
+
+        dataEvents = Array.isArray(dataEvents) ? dataEvents : [dataEvents];
+        endEvents = Array.isArray(endEvents) ? endEvents : [endEvents];
+        errorEvents = Array.isArray(errorEvents) ? errorEvents : [errorEvents];
+        this.pause();
+        for (const name of [...dataEvents, ...endEvents, ...errorEvents]) {
+            this.readable.on(name, (...args: any[]) => this._emitter.emit('any', {name, args}));
+        }
+        for (const name of dataEvents) {
+            this.readable.on(name, (...args: any[]) => this.eventHolder.push({type: DATA, args}));
+        }
+        for (const name of endEvents) {
+            this.readable.on(name, (...args: any[]) => this.eventHolder.push({type: END, args}));
+        }
+        for (const name of errorEvents) {
+            this.readable.on(name, (...args: any[]) => this.eventHolder.push({type: ERROR, args}));
+        }
+    }
+
+    private isReadableMethod(methodName: string): boolean {
+        return isFunction((this.readable as any)[methodName]);
+    }
+
+    private callReadableMethod<T = void>(methodName: string): T {
+        return ((this.readable as any)[methodName] as () => T)();
+    }
+
+    pause(): Reader {
+        if (this._isPaused && this.isReadableMethod(this._isPaused) &&
+            this.callReadableMethod<boolean>(this._isPaused)) {
+            return this;
+        }
+
+        if (this.isReadableMethod(this._pause)) {
+            debug('pause');
+            this.callReadableMethod(this._pause);
+        }
+
+        return this;
+    }
+
+    resume(): Reader {
+        if (this._isPaused && this.isReadableMethod(this._isPaused) &&
+            !this.callReadableMethod<boolean>(this._isPaused)) {
+            return this;
+        }
+
+        if (this.isReadableMethod(this._resume)) {
+            debug('resume');
+            this.callReadableMethod(this._resume)
+        }
+
+        return this;
+    }
+
+    _emitted(): Promise<any> {
+        return new Promise(resolve => this._emitter.once('any', resolve));
+    }
+
+    async nextEvent(): Promise<Event> {
+        debug('next event');
+        if (this.eventHolder.length > 0) {
+            this.pause();
+            return this.eventHolder.shift()!;
+        }
+        this.resume();
+        await this._emitted();
+        return await this.nextEvent();
+    }
+
+    async next(): Promise<any> {
+        const {args, type} = await this.nextEvent();
+        debug('next', args, type);
+        const result = args[0];
+        if (type === Reader.ERROR) {
+            debug('error');
+            throw result;
+        }
+        if (type === Reader.END) {
+            debug('end');
+            return this._endArgument;
+        }
+        return result;
+    }
+}
+
+
+module.exports = Reader;
